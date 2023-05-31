8e785d9e790cc132cd8e6c0eff111e33efb7a744
abbos <abbosvaliev2003@gmail.com>
2023-05-31 15:23:15 +0500

    feat: fixes v1

--- js/scripts.js
+++ js/scripts.js
@@ -25,34 +25,34 @@ accordion()
 
 
 // Модальное окно
-function bindModal(trigger, modal, close) {
-    trigger = document.querySelector(trigger),
-    modal = document.querySelector(modal),
-    close = document.querySelector(close)
+// function bindModal(trigger, modal, close) {
+//     trigger = document.querySelector(trigger),
+//     modal = document.querySelector(modal),
+//     close = document.querySelector(close)
 
-    const body = document.body
+//     const body = document.body
 
-    trigger.addEventListener('click', e => {
-        e.preventDefault()
-        modal.style.display = 'flex'
-        body.classList.add('locked')
-    });
-    close.addEventListener('click', () => {
-        modal.style.display = 'none'
-        body.classList.remove('locked')
-    });
-    modal.addEventListener('click', e => {
-        if (e.target === modal) {
-        modal.style.display = 'none'
-        body.classList.remove('locked')
-        }
-    })
-}
+//     trigger.addEventListener('click', e => {
+//         e.preventDefault()
+//         modal.style.display = 'flex'
+//         body.classList.add('locked')
+//     });
+//     close.addEventListener('click', () => {
+//         modal.style.display = 'none'
+//         body.classList.remove('locked')
+//     });
+//     modal.addEventListener('click', e => {
+//         if (e.target === modal) {
+//         modal.style.display = 'none'
+//         body.classList.remove('locked')
+//         }
+//     })
+// }
 
-// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
-// ВТОРОЙ аргумент - класс самого модального окна.
-// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
-bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+// // ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// // ВТОРОЙ аргумент - класс самого модального окна.
+// // ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+// bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
 
 
 const swiperReview = new Swiper('.review__swiper', {
@@ -83,4 +83,44 @@ const swiperReview = new Swiper('.review__swiper', {
       spaceBetween: 30
     },
   }
+});
+
+
+/* JavaScript for smooth scrolling */
+const smoothScroll = function (target, duration) {
+  const targetEl = document.querySelector(target);
+  const targetPos = targetEl.getBoundingClientRect().top;
+  const startPos = window.pageYOffset;
+  let startTime = null;
+
+  const animation = function (currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPos, targetPos, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  };
+
+  const ease = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  requestAnimationFrame(animation);
+};
+
+const links = document.querySelectorAll("a");
+
+links.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    const currentLink = item.getAttribute("href");
+    smoothScroll(currentLink, 1000);
+  });
 });
