f89e140693c3c80449ff2dd0a0982afe0ff642fe
abbos <abbosvaliev2003@gmail.com>
2023-06-16 15:15:43 +0500

    feat: add magnific popup

--- js/scripts.js
+++ js/scripts.js
@@ -24,37 +24,6 @@ function accordion() {
 accordion() 
 
 
-// Модальное окно
-// function bindModal(trigger, modal, close) {
-//     trigger = document.querySelector(trigger),
-//     modal = document.querySelector(modal),
-//     close = document.querySelector(close)
-
-//     const body = document.body
-
-//     trigger.addEventListener('click', e => {
-//         e.preventDefault()
-//         modal.style.display = 'flex'
-//         body.classList.add('locked')
-//     });
-//     close.addEventListener('click', () => {
-//         modal.style.display = 'none'
-//         body.classList.remove('locked')
-//     });
-//     modal.addEventListener('click', e => {
-//         if (e.target === modal) {
-//         modal.style.display = 'none'
-//         body.classList.remove('locked')
-//         }
-//     })
-// }
-
-// // ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
-// // ВТОРОЙ аргумент - класс самого модального окна.
-// // ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
-// bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
-
-
 const swiperReview = new Swiper('.review__swiper', {
     slidesPerView: 1,
     spaceBetween: 20,
@@ -114,41 +83,43 @@ const swiperAbout = new Swiper('.about__swiper', {
 });
 
 
-// /* JavaScript for smooth scrolling */
-// const smoothScroll = function (target, duration) {
-//   const targetEl = document.querySelector(target);
-//   const targetPos = targetEl.getBoundingClientRect().top;
-//   const startPos = window.pageYOffset;
-//   let startTime = null;
 
-//   const animation = function (currentTime) {
-//     if (startTime === null) {
-//       startTime = currentTime;
-//     }
-//     const timeElapsed = currentTime - startTime;
-//     const run = ease(timeElapsed, startPos, targetPos, duration);
-//     window.scrollTo(0, run);
-//     if (timeElapsed < duration) {
-//       requestAnimationFrame(animation);
-//     }
-//   };
 
-//   const ease = function (t, b, c, d) {
-//     t /= d / 2;
-//     if (t < 1) return (c / 2) * t * t + b;
-//     t--;
-//     return (-c / 2) * (t * (t - 2) - 1) + b;
-//   };
+// MAGNIFIC
 
-//   requestAnimationFrame(animation);
-// };
+$(document).ready(function() {
+  // Инициализируем Magnific Popup
+  $('.modal-link').magnificPopup({
+    type: 'inline'
+  });
 
-// const links = document.querySelectorAll("a");
+  // Функция для открытия модального окна 1
+  $('#btn1').on('click', function() {
+    $.magnificPopup.open({
+      items: {
+        src: '#modal1'
+      },
+      type: 'inline'
+    });
+  });
 
-// links.forEach((item) => {
-//   item.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const currentLink = item.getAttribute("href");
-//     smoothScroll(currentLink, 1000);
-//   });
-// });
+  // Функция для открытия модального окна 2
+  $('#btn2').on('click', function() {
+    $.magnificPopup.open({
+      items: {
+        src: '#modal2'
+      },
+      type: 'inline'
+    });
+  });
+
+  // Функция для открытия модального окна 3
+  $('#btn3').on('click', function() {
+    $.magnificPopup.open({
+      items: {
+        src: '#modal3'
+      },
+      type: 'inline'
+    });
+  });
+});
